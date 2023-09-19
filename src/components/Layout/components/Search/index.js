3c263323d3b03e456196309174f0a8b68c9d20e4
DNAnh01 <donguyenanhgithub@gmail.com>
2023-09-19 19:03:03 +0700

    Bài 74: Xây dựng Logic phần Header #3

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -9,6 +9,7 @@ import classNames from 'classnames/bind';
 import styles from './Search.module.scss';
 import { useEffect, useState, useRef } from 'react';
 import { SearchIcon } from '~/components/Icons';
+import { useDebounce } from '~/hooks';
 
 const cx = classNames.bind(styles);
 function Search() {
@@ -16,17 +17,18 @@ function Search() {
     const [searchResult, setSearchResult] = useState([]);
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
+    const debounced = useDebounce(searchValue, 500);
 
     const inputRef = useRef();
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([]);
             return;
         }
         setLoading(true);
         // Call API
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then((res) => res.json())
             .then((res) => {
                 setSearchResult(res.data);
@@ -35,7 +37,7 @@ function Search() {
             .catch(() => {
                 setLoading(false);
             });
-    }, [searchValue]);
+    }, [debounced]);
 
     const handelClear = () => {
         setSearchValue('');
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,7 @@
-import { useParams } from 'react-router-dom';
-
 function Profile() {
-    let { nickname } = useParams();
     return (
         <div>
             <h2>Profile page</h2>
-            <p>Nickname: {nickname}</p>
         </div>
     );
 }
