70bc14adb7a17570e281e04d3e7484cdda819b8c
DNAnh01 <donguyenanhgithub@gmail.com>
2023-09-21 13:02:02 +0700

    Bài 75: Tìm hiểu và sử dụng thư viện Axios

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -1,13 +1,14 @@
 import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import HeadlessTippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState, useRef } from 'react';
 
+import * as searchServices from '~/apiServices/searchServices';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 
 import classNames from 'classnames/bind';
 import styles from './Search.module.scss';
-import { useEffect, useState, useRef } from 'react';
 import { SearchIcon } from '~/components/Icons';
 import { useDebounce } from '~/hooks';
 
@@ -26,17 +27,14 @@ function Search() {
             setSearchResult([]);
             return;
         }
-        setLoading(true);
-        // Call API
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
-            .then((res) => res.json())
-            .then((res) => {
-                setSearchResult(res.data);
-                setLoading(false);
-            })
-            .catch(() => {
-                setLoading(false);
-            });
+
+        const fetchApi = async () => {
+            setLoading(true);
+            const result = await searchServices.search(debounced);
+            setSearchResult(result);
+            setLoading(false);
+        };
+        fetchApi();
     }, [debounced]);
 
     const handelClear = () => {
