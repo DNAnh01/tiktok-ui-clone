327e8fb4959f381f71bf31562cb3c64898b102d0
DNAnh01 <donguyenanhgithub@gmail.com>
2023-09-23 22:44:24 +0700

    Bài 81: Giải quyết vấn đề nhiều file index.js

---
+++ src/components/Popper/Menu/Menu.js
@@ -0,0 +1,88 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
+
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+
+import MenuItem from './MenuItem';
+import Header from './Header';
+import styles from './Menu.module.scss';
+import { useState } from 'react';
+
+const cx = classNames.bind(styles);
+const defaultFn = () => {};
+
+/**
+ *  The first time you hover over the parent menu, it is the first element of an original array.
+ *  When selecting an element in the parent array, a sub-array appears. Then, the entire sub-array
+ *  is treated as a single element and is pushed into the original array. Assuming there are grandchildren,
+ *  we push it into the third element of the array again.
+ *
+ *  And at the current moment, we always render the last element of the original array, and when we go back,
+ *  we only need to remove the last element.
+ */
+
+function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn }) {
+    const [history, setHistory] = useState([{ data: items }]);
+
+    const currentMenu = history[history.length - 1];
+
+    const renderItems = () => {
+        return currentMenu.data.map((item, index) => {
+            const isParent = !!item.children;
+
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            setHistory((prev) => {
+                                return [...prev, item.children];
+                            });
+                        } else {
+                            onChange(item);
+                        }
+                    }}
+                />
+            );
+        });
+    };
+
+    return (
+        <Tippy
+            interactive
+            delay={[0, 700]}
+            offset={[16, 8]}
+            hideOnClick={hideOnClick}
+            placement="bottom-end"
+            render={(attrs) => (
+                <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+                    <PopperWrapper className={cx('menu-popper')}>
+                        {history.length > 1 && (
+                            <Header
+                                title={currentMenu.title}
+                                onBack={() => {
+                                    setHistory((prev) => prev.slice(0, prev.length - 1));
+                                }}
+                            />
+                        )}
+                        <div className={cx('menu-body')}>{renderItems()}</div>
+                    </PopperWrapper>
+                </div>
+            )}
+            onHide={() => setHistory((prev) => prev.slice(0, 1))}
+        >
+            {children}
+        </Tippy>
+    );
+}
+
+Menu.propTypes = {
+    children: PropTypes.node.isRequired,
+    items: PropTypes.array,
+    hideOnClick: PropTypes.bool,
+    onChange: PropTypes.func,
+};
+
+export default Menu;
